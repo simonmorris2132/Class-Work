ce513b483e99835d8ed91ca910fba0dc78e3c5d1
unknown <simonjmorris213@gmail.com>
2021-07-12 21:16:19 -0700

    oop_research2

--- 2021_07/OOP_research2.js
+++ 2021_07/OOP_research2.js
@@ -16,25 +16,44 @@ function getVoteCount(votes) {
 	
 }
 
-/*  */
+/* 
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
 
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+*/
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
 
 function whatIsInAName(collection, source) {
     var arr = [];
     // Only change code below this line
-  
+  /* 
     collection.forEach( testObj => {
         console.log(testObj)
-    } )
+        let passedTest = true
+        if (passedTest == true) { //this also works
+            arr.push(testObj)
+        }
 
-   /*  for (let i = 0; i < collection.length; i++) {
-        const testObj = collection[i]
+    } ) */
+
+
+
+    for (let i = 0; i < collection.length; i++) {
+        const testObj = collection[i];
+
+        let passedTest = true;
+        console.log(source, 'test:',testObj)
+        if (testObj != source) {
+            passedTest = false;
+        }
         
-        console.log(testObj) //this also works
+        if (passedTest == true) {
+            arr.push(testObj)
+        }
+
     }
- */
     // Only change code above this line
     return arr;
   }
   
-  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
