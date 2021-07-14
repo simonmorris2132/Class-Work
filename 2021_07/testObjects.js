218278d3eb7e5cc45909b07356121888e9ae85a1
unknown <simonjmorris213@gmail.com>
2021-07-13 19:09:51 -0700

    classStuff

---
+++ 2021_07/testObjects.js
@@ -0,0 +1,15 @@
+const fruitNames = ['Apple', 'Kiwi', 'Coconut']
+const fruitPrices = [1, .5, 3]
+const fruitSales = [10, 20, 5]
+
+const fruits = []
+
+for (let i = 0; i < fruitNames.length; i++) {
+    
+    const fruitObj = {Name: fruitNames[i], Price: fruitPrices[i], Sales: fruitSales[i]}
+
+    fruits.push(fruitObj)
+
+}
+
+console.log(fruits)
