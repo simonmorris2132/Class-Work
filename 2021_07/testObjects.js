d5347d6fd58d9d95e1a91a9c016c6ef15339f20e
unknown <simonjmorris213@gmail.com>
2021-07-13 20:41:16 -0700

    classStuff

--- 2021_07/testObjects.js
+++ 2021_07/testObjects.js
@@ -21,3 +21,28 @@ fruits.forEach((fruit, i) => {
 })
 
 console.log(fruits)
+
+
+const members = ['j', 'm', 't', 'g', 's', 'h', 'y']
+
+const team1 = []
+const team2 = []
+
+for (let i = 0; i < members.length; i++) {
+    if (Math.random() > 5) {
+        team1.push(members[i])
+    } else {
+        team2.push(members[i])
+    }
+    
+}
+
+while (team1.length > 4) {
+    team2.push(team1.pop())
+}
+
+while (team2.length > 4) {
+    team1.push(team2.pop())
+}
+
+console.log(team1, team2)
