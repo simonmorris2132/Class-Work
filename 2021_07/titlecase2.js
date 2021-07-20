62cf49a9fd30d6979012291908928c4534debc70
Simon Morris <simonjmorris213@gmail.com>
2021-07-19 21:50:03 -0700

    titlecase

--- 2021_07/titlecase2.js
+++ 2021_07/titlecase2.js
@@ -21,10 +21,10 @@
   function splitStr(value, arg) {
     let str = []
     
-    for (let i = 0; i < value.length; i++) { //use a while loop too dum dum
+    for (let i = 0; i < value.length; i++) { 
         if (value.slice(i, i + arg.length) == arg) {
             str.push(value.slice(0, i)) /* pushing into empty array str and trying to find a space in the value.*/
-            value = value.slice(i + arg.length) // could also put in value.length
+            value = value.slice(i + arg.length) /* could also put in value.length */
            console.log(i, value)
            i = 0
         }
