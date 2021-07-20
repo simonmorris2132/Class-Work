c0f83e813403445167265dceb63643c20240d562
Simon Morris <simonjmorris213@gmail.com>
2021-07-19 18:59:20 -0700

    first java project

--- 2021_07/FirstJavaProject/src/com/company/Main.java
+++ 2021_07/FirstJavaProject/src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Simon");
+        System.out.print("Simon");
     }
 }
