46bc4679ce0fd2b57b40cca4e88fb3ec1a597ee6
Simon Morris <simonjmorris213@gmail.com>
2021-07-20 21:07:14 -0700

    did a .split thing from javascript and then doing a .join thing to put it all back together.

--- 2021_07/FirstJavaProject/src/com/company/Main.java
+++ 2021_07/FirstJavaProject/src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
     /* modifiers are basically things that give access to classes, fields, or methods. static is a different kind of modifier. We can use fields without stating the class. Void means that after the program runs, nothing else should follow. Void is a return-type. main() is the name of the function, and inside of it's parenthesis is the arguments. */
     public static void main(String[] args) {
 
-        titleCase("i am a developer");
+        System.out.println(titleCase("i am a developer")); //printing it to console
 
     }
 
@@ -24,16 +24,18 @@ public class Main {
         String[] splitInput = input.split(" ");
 
         for (int i =0; i < splitInput.length; i++) {
-            System.out.println(splitInput[i].toUpperCase().substring(0, 1) + "" + splitInput[i].toLowerCase().substring(1)); /*pushing each word in the string from the main method and making each first letter a capital*/
+            //System.out.println(splitInput[i].toUpperCase().substring(0, 1) + "" + splitInput[i].toLowerCase().substring(1)); /*pushing each word in the string from the main method and making each first letter a capital*/
             splitInput[i] = splitInput[i].toUpperCase().substring(0, 1) + "" + splitInput[i].toLowerCase().substring(1); /*assigning the previous code to the splitInput variable splitInput[i] = i + " ";*/
 
         }
-        System.out.println(splitInput);
+        //System.out.println(splitInput);
         String newStr = "";
         for (int i = 0; i < splitInput.length; i++) {
-            newStr = splitInput[i];
+            //System.out.println(splitInput[i]);
+            newStr += splitInput[i] + " "; // putting it together
         }
-        return "";
+
+        return newStr;
     }
 
 }
