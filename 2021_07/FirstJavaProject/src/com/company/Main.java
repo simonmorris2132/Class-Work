ef3daefa25d67db250bdd00c3724a623b26dd280
Simon Morris <simonjmorris213@gmail.com>
2021-07-20 17:49:43 -0700

    first java project

--- 2021_07/FirstJavaProject/src/com/company/Main.java
+++ 2021_07/FirstJavaProject/src/com/company/Main.java
@@ -1,8 +1,6 @@
 package com.company;
 
 
-import java.util.Locale;
-
 public class Main {
     /* modifiers are basically things that give access to classes, fields, or methods. static is a different kind of modifier. We can use fields without stating the class. Void means that after the program runs, nothing else should follow. Void is a return-type. main() is the name of the function, and inside of it's parenthesis is the arguments. */
     public static void main(String[] args) {
