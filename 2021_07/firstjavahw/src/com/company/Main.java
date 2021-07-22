05b8a403bdf5a93c486f20ec96f7326e53f2cbc0
Simon Morris <simonjmorris213@gmail.com>
2021-07-22 02:09:29 -0700

    Conversion to fahrenheit

---
+++ 2021_07/firstjavahw/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//initializing
+        double celsius = 10.0, fahrenheit = 0.0;
+
+        //conversion
+        fahrenheit = (celsius * 1.7) + 32;
+        System.out.println("The value of temperature in fahrenheit is: " + fahrenheit);
+    }
+}
