36e03c8dd0420b4cbc81928dbfe4c89c8a3c7862
Simon Morris <simonjmorris213@gmail.com>
2021-07-22 18:41:23 -0700

    Reversing a string using the StringBuilder class

---
+++ 2021_07/reverseastring/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String input = "Hello";
+
+        StringBuilder input1 = new StringBuilder();
+
+        //put string into this method
+        input1.append(input);
+
+        //reverse the string
+        input1.reverse();
+
+        //print
+        System.out.println(input1);
+        }
+    }
