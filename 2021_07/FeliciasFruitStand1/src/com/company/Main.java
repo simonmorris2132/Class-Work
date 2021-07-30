149b3eab93648cce323046442884a4518315be7f
Simon Morris <simonjmorris213@gmail.com>
2021-07-29 21:32:47 -0700

    fruitstand

--- 2021_07/FeliciasFruitStand1/src/com/company/Main.java
+++ 2021_07/FeliciasFruitStand1/src/com/company/Main.java
@@ -5,7 +5,7 @@ package com.company;
 Create a new InteliJ project called "FeliciasFruitStand"
 
 Objects
-Create a Fruit class that can has properties for price, name, quantity available and quantity sold
+Create a fruit class that can has properties for price, name, quantity available and quantity sold
 
 fruit object: price, name, quantity available and quantity sold
 fruit class methods: Override toString(returns an output with all of a fruits' info), purchased(update sold quantity), addInventory(update available quantity).
@@ -18,10 +18,10 @@ stand class methods: displayFruit(prints the info of all fruits available).
 Methods
 
 
-The Fruit class should have an "Override" toString method that returns an output with all of a Fruits information
+The fruit class should have an "Override" toString method that returns an output with all of a Fruits information
 
 
-The Fruit class should also include methods called "purchase" & "addInventory" that update the available and sold quantity of the Fruit
+The fruit class should also include methods called "purchase" & "addInventory" that update the available and sold quantity of the fruit
 
 
 The Stand class should have a method called displayFruit that prints the information of all Fruits available */
@@ -29,6 +29,15 @@ The Stand class should have a method called displayFruit that prints the informa
 public class Main {
 
     public static void main(String[] args) {
+        //Fruit(String name, int price, int qAvail, int qSold)
+        Fruit fruit1 = new Fruit("Apple", 250, 57, 120);
+        Fruit fruit2 = new Fruit("Mango", 300, 63, 50);
+        Fruit fruit3 = new Fruit("Oranges", 350, 70, 66);
+        fruit1.addInventory(5);
+        System.out.println(fruit1.getqAvailable());
+
+        fruit2.purchase(10);
+        System.out.println(fruit2.getqAvailable());
 
     }
 }
--- 2021_07/FeliciasFruitStand1/src/com/company/Stand.java
+++ 2021_07/FeliciasFruitStand1/src/com/company/Stand.java
@@ -3,9 +3,15 @@ package com.company;
 /*
 stand object: inventory
 stand class methods: displayFruit(prints the info of all fruits available).
-Data types: int, object (explicit casting...calling itself and all classes)
+Data types: int, object (explicit casting...calling itself and all classes), strings
 ex: Stand.inventory
 */
 
 public class Stand {
+    private Fruit[] inventory;
+
+    public Stand(Fruit[] inv) {
+        this.inventory = inv;
+    }
+
 }
