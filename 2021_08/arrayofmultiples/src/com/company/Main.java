69297cc302d6c30ebe30486c7073e61ae2a87624
Simon Morris <simonjmorris213@gmail.com>
2021-08-10 20:26:12 -0700

    stuff

--- 2021_08/arrayofmultiples/src/com/company/Main.java
+++ 2021_08/arrayofmultiples/src/com/company/Main.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class Main extends ArrayOfMul {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(arrayOfMultiples(7, 5)));
+        System.out.println(Arrays.toString(arrayOfMultiples(5, 7)));
     }
 }
