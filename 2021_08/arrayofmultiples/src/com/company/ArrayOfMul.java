570cf45de153f0a60a439d03e103cee15eee56ee
Simon Morris <simonjmorris213@gmail.com>
2021-08-10 20:12:54 -0700

    javastuff

--- 2021_08/arrayofmultiples/src/com/company/ArrayOfMul.java
+++ 2021_08/arrayofmultiples/src/com/company/ArrayOfMul.java
@@ -3,7 +3,7 @@ package com.company;
 public class ArrayOfMul {
     public static int[] arrayOfMultiples(int num, int length) {
         int[] array = new int[length];
-        for (int i = 0; i < length; ++i) {
+        for (int i = 0; i < length; i++) {
             array[i] = num * i;
         }
         return array;
