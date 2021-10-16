dccf7589acbee6df48307c429dd395e2b27dd31c
Simon Morris <simonjmorris213@gmail.com>
2021-10-16 01:38:31 -0700

    classwork

--- 2021_09/InterChallenge/SearchAndReplace/Main.java
+++ 2021_09/InterChallenge/SearchAndReplace/Main.java
@@ -1,5 +1,7 @@
 package InterChallenge.SearchAndReplace;
 
+@SuppressWarnings("ALL")
+
 public class Main {
     public static void main(String[] args) {
         
--- 2021_09/InterChallenge/SearchAndReplace/SearchAndReplace.java
+++ 2021_09/InterChallenge/SearchAndReplace/SearchAndReplace.java
@@ -1,12 +1,14 @@
 package InterChallenge.SearchAndReplace;
 
+@SuppressWarnings("ALL")
+
 public class SearchAndReplace {
 
     public static void run() {
         
         String searchThru = "The quick brown fox jumped over the brown log.";
         String replaceWith = "leaped";
-        
+
     }
     
 }
