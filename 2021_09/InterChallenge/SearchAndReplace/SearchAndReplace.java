55fe20e31a0d011a0952b9077f455e74987597cf
Simon Morris <simonjmorris213@gmail.com>
2021-10-16 01:38:00 -0700

    classwork

--- 2021_09/InterChallenge/SearchAndReplace/SearchAndReplace.java
+++ 2021_09/InterChallenge/SearchAndReplace/SearchAndReplace.java
@@ -2,6 +2,11 @@ package InterChallenge.SearchAndReplace;
 
 public class SearchAndReplace {
 
-    
+    public static void run() {
+        
+        String searchThru = "The quick brown fox jumped over the brown log.";
+        String replaceWith = "leaped";
+        
+    }
     
 }
