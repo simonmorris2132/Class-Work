cbac299cb17a18a3fcb5cc82d8a5cba7be3a530a
Simon Morris <simonjmorris213@gmail.com>
2021-11-02 15:31:45 -0700

    firstServer

--- 2021_10/firstServer/src/main/java/com/example/firstServer/service/PersonService.java
+++ 2021_10/firstServer/src/main/java/com/example/firstServer/service/PersonService.java
@@ -3,6 +3,7 @@ package com.example.firstServer.service;
 import com.example.firstServer.dao.PersonDao;
 import com.example.firstServer.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +12,7 @@ public class PersonService {
     private final PersonDao personDao;
 
     @Autowired
-    public PersonService(PersonDao personDao) {
+    public PersonService(@Qualifier("Fake") PersonDao personDao) {
         this.personDao = personDao;
     }
 
