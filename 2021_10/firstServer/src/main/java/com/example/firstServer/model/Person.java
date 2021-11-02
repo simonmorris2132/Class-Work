c5e49e6a5a8504b528c81193419551b645e72896
Simon Morris <simonjmorris213@gmail.com>
2021-11-02 15:32:49 -0700

    firstServer

--- 2021_10/firstServer/src/main/java/com/example/firstServer/model/Person.java
+++ 2021_10/firstServer/src/main/java/com/example/firstServer/model/Person.java
@@ -5,14 +5,18 @@ import java.util.UUID;
 public class Person {
     private final UUID id;
     private final String name;
+
     public Person(UUID id, String name) {
         this.id = id;
         this.name = name;
     }
+
     public UUID getId() {
         return this.id;
     }
+
     public String getName() {
         return this.name;
     }
+    
 }
