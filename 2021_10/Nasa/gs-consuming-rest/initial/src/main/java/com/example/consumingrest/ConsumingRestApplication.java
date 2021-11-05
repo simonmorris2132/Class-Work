1bf1492612088eb2b535ad6febb6b0529fe1fc37
Simon Morris <simonjmorris213@gmail.com>
2021-11-04 21:21:40 -0700

    classWork

--- 2021_10/Nasa/gs-consuming-rest/initial/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ 2021_10/Nasa/gs-consuming-rest/initial/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -1,20 +1,35 @@
 package com.example.consumingrest;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class ConsumingRestApplication {
+
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumingRestApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
+
+    @Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"https://quoters.apps.pcfone.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
 
 }
--- 2021_10/firstServer/src/main/java/com/example/firstServer/FirstServerApplication.java
+++ 2021_10/firstServer/src/main/java/com/example/firstServer/FirstServerApplication.java
@@ -2,12 +2,8 @@ package com.example.firstServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class FirstServerApplication {
   
 	
@@ -15,10 +11,5 @@ public class FirstServerApplication {
 	SpringApplication.run(FirstServerApplication.class, args);
 	}
 	
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
-	}
-  
 }
 
