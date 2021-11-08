4efa95661c6832c250465f6ef4178ac08c3c2fee
Simon Morris <simonjmorris213@gmail.com>
2021-11-08 15:00:34 -0800

    classWork

---
+++ 2021_11/gateway/src/main/java/com/example/gateway/APOD.java
@@ -0,0 +1,9 @@
+package com.example.gateway;
+
+public class APOD {
+    private String title;
+    private String url;
+    private String explanation;
+
+    
+}
--- 2021_11/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ 2021_11/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,12 +2,18 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String rootRoute() {
+		return "Welcome Home!";
 	}
 
 }
