83415622cc60311e1525dbe67cd1bde36721f882
Simon Morris <simonjmorris213@gmail.com>
2021-11-08 15:05:38 -0800

    classWork

--- 2021_11/gateway/src/main/java/com/example/gateway/APOD.java
+++ 2021_11/gateway/src/main/java/com/example/gateway/APOD.java
@@ -1,9 +1,34 @@
 package com.example.gateway;
 
 public class APOD {
+    
     private String title;
     private String url;
     private String explanation;
 
-    
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getExplanation() {
+        return this.explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
 }
--- 2021_11/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ 2021_11/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,13 +2,23 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
 	@GetMapping("/")
