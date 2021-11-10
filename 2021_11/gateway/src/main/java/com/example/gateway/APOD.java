3a9a1b6dcd3cb1e49c4f0b81a12c9450df63879d
simonmorris2132 <simonjmorris213@gmail.com>
2021-11-09 21:25:49 -0800

    classWork

--- 2021_11/gateway/src/main/java/com/example/gateway/APOD.java
+++ 2021_11/gateway/src/main/java/com/example/gateway/APOD.java
@@ -7,6 +7,8 @@ public class APOD {
     private String date;
     private String title;
     private String url;
+    private String hdurl;
+    private String copyright;
     private String explanation;
 
     public String getDate() {
@@ -31,6 +33,22 @@ public class APOD {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public String getHdurl() {
+        return hdurl;
+    }
+
+    public void setHdurl(String hdurl) {
+        this.hdurl = hdurl;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
     }
 
     public String getExplanation() {
