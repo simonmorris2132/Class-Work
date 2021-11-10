f2ca854bf5e63aadaad8b736fd9b37128c5379df
Simon Morris <simonjmorris213@gmail.com>
2021-11-09 16:59:44 -0800

    classWork

--- 2021_11/gateway/src/main/java/com/example/gateway/APOD.java
+++ 2021_11/gateway/src/main/java/com/example/gateway/APOD.java
@@ -1,11 +1,21 @@
 package com.example.gateway;
 
+import java.util.Set;
+
 public class APOD {
     
+    private String date;
     private String title;
     private String url;
     private String explanation;
 
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 
     public String getTitle() {
         return this.title;
