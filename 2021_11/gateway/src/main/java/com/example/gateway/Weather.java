f968e8e246a49f3bde9e8e90a6a9abfc06e105da
simonmorris2132 <simonjmorris213@gmail.com>
2021-11-11 20:32:26 -0800

    commit

--- 2021_11/gateway/src/main/java/com/example/gateway/Weather.java
+++ 2021_11/gateway/src/main/java/com/example/gateway/Weather.java
@@ -5,9 +5,43 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Weather {
     
+    private String cod;
+    private String message;
+    private String cnt;
+    private String list;
     private String city;
-    private String statecode;
-    private String weather;
+
+    public String getCod() {
+        return this.cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCnt() {
+        return this.cnt;
+    }
+
+    public void setCnt(String cnt) {
+        this.cnt = cnt;
+    }
+
+    public String getList() {
+        return this.list;
+    }
+
+    public void setList(String list) {
+        this.list = list;
+    }
 
     public String getCity() {
         return this.city;
@@ -15,22 +49,6 @@ public class Weather {
 
     public void setCity(String city) {
         this.city = city;
-    }
-
-    public String getStatecode() {
-        return this.statecode;
-    }
-
-    public void setStatecode(String statecode) {
-        this.statecode = statecode;
-    }
-
-    public String getWeather() {
-        return this.weather;
-    }
-
-    public void setWeather(String weather) {
-        this.weather = weather;
     }
 
 }
