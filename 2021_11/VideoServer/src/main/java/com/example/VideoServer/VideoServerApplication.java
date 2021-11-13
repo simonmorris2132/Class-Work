af706f33e8d75160a5490b62adda964cebccb702
Simon Morris <simonjmorris213@gmail.com>
2021-11-13 11:03:55 -0800

    commit

---
+++ 2021_11/VideoServer/src/main/java/com/example/VideoServer/VideoServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.VideoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoServerApplication.class, args);
+	}
+
+}
