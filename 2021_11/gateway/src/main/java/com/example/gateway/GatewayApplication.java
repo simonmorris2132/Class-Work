f62f5c947f207cf3f00c656ec70528e7a05162b3
Simon Morris <simonjmorris213@gmail.com>
2021-11-13 10:52:23 -0800

    helpmelord

--- 2021_11/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ 2021_11/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -36,7 +36,7 @@ public class GatewayApplication {
 
 	}
 
-	@GetMapping("/weather") /* Returns with weather route and displays all info from the free weather api for providence rhode island */
+	@GetMapping("/weather") /* Returns with weather route and displays all info from the free weather api */
 	public Weather weatherInfo(RestTemplate restTemplate) {
 		String URL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=cfc27e811208976be0555ea9425aec24";
 
