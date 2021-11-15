c8c4791594ab546e4225df0c784e91006c097091
simonmorris2132 <simonjmorris213@gmail.com>
2021-11-14 20:43:51 -0800

    commit

--- 2021_11/ServerFromVideo/src/main/java/com/example/ServerFromVideo/API/PersonController.java
+++ 2021_11/ServerFromVideo/src/main/java/com/example/ServerFromVideo/API/PersonController.java
@@ -3,7 +3,10 @@ package com.example.ServerFromVideo.API;
 import com.example.ServerFromVideo.Model.Person;
 import com.example.ServerFromVideo.Service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PersonController {
     
     private final PersonService personService;
@@ -13,6 +16,7 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @PostMapping
     public void addPerson(Person person) {
         personService.addPerson(person);
     }
