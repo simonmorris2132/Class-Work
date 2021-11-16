8c16f4da31b4657c905f87fba7f369cdc6bddcce
simonmorris2132 <simonjmorris213@gmail.com>
2021-11-15 20:38:14 -0800

    commit

--- 2021_11/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ 2021_11/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -38,9 +38,9 @@ public class GatewayApplication {
 
 	@GetMapping("/weather") /* Returns with weather route and displays all info from the free weather api */
 	public Weather weatherInfo(RestTemplate restTemplate) {
-		String URL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=cfc27e811208976be0555ea9425aec24";
+		String URL1 = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=cfc27e811208976be0555ea9425aec24";
 
-		Weather weather = restTemplate.getForObject(URL, Weather.class);
+		Weather weather = restTemplate.getForObject(URL1, Weather.class);
 
 		return weather;
 	}
--- 2021_11/gateway/src/main/java/com/example/gateway/Weather.java
+++ 2021_11/gateway/src/main/java/com/example/gateway/Weather.java
@@ -1,16 +1,11 @@
 package com.example.gateway;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Weather {
     
     private String cod;
     private String message;
     private String cnt;
     private String list;
-    private String city;
-    private String weather;
 
     public String getCod() {
         return this.cod;
@@ -42,22 +37,6 @@ public class Weather {
 
     public void setList(String list) {
         this.list = list;
-    }
-
-    public String getCity() {
-        return this.city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getWeather() {
-        return this.weather;
-    }
-
-    public void setWeather(String weather) {
-        this.weather = weather;
     }
 
 }
