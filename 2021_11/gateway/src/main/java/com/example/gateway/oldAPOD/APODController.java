6c6f19637dd72204fd37eeeb796fb58e9a82c6e1
simonmorris2132 <simonjmorris213@gmail.com>
2021-11-17 22:13:59 -0800

    class

--- 2021_11/gateway/src/main/java/com/example/gateway/oldAPOD/APODController.java
+++ 2021_11/gateway/src/main/java/com/example/gateway/oldAPOD/APODController.java
@@ -3,18 +3,16 @@ package com.example.gateway.OldAPOD;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/apod")
 public class APODController {
 	
 	@Autowired
 	private Environment env;
     
-    @GetMapping("/info")
+    @GetMapping("/apodinfo")
 	public APOD apodInfo(RestTemplate restTemplate) {
 		String URL = "https://api.nasa.gov/planetary/apod?api_key=" + env.getProperty("apikey.nasa");
 
