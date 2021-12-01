89dfafbe98df07539060da093a221bad67faadb4
Simon Morris <simonjmorris213@gmail.com>
2021-11-30 18:58:42 -0800

    commit

--- 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUserController.java
+++ 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUserController.java
@@ -3,17 +3,27 @@ package com.example.JSONPlaceHolder;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class JPUserController {
     
-@GetMapping("/api/db/1")
-public JPUser jpUserInfo(RestTemplate restTemplate) {
-    String URL = "https://jsonplaceholder.typicode.com/users/1";
+@GetMapping("/allusers")
+public JPUser[] jpUserInfo(RestTemplate restTemplate) {
+    String URL = "https://jsonplaceholder.typicode.com/users";
 
-    JPUser jpUser = restTemplate.getForObject(URL, JPUser.class);
+    JPUser[] jpUser = restTemplate.getForObject(URL, JPUser[].class);
 
     return jpUser;
+}
+
+@GetMapping("/oneuser/{id}")
+public JPUser jpUserinfo2(RestTemplate restTemplate, @PathVariable short id) {
+    String URL2 = "https://jsonplaceholder.typicode.com/users" + id;
+    
+    JPUser jpUsers = restTemplate.getForObject(URL2, JPUser.class);
+    
+    return jpUsers;
 }
 
 
