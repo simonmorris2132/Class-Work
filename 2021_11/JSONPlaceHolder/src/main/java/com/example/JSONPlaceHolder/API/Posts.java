3e779a5bdba3973fc78dafd6c250673daa04982c
Simon Morris <simonjmorris213@gmail.com>
2021-11-30 17:59:42 -0800

    commit

--- 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/API/Posts.java
+++
@@ -1,6 +0,0 @@
-package com.example.JSONPlaceHolder.API;
-
-public class Posts {
-    public short id;
-    public String title;
-}
---
+++ 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUser.java
@@ -0,0 +1,41 @@
+package com.example.JSONPlaceHolder;
+
+public class JPUser {
+    private short id;
+    private String name;
+    private String username;
+    private String email;
+
+    public short getId() {
+        return this.id;
+    }
+
+    public void setId(short id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
