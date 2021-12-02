360ebabf38559b9208e68dd5817d84396126223e
Simon Morris <simonjmorris213@gmail.com>
2021-12-02 15:00:24 -0800

    commit

--- 2021_11/GitHubAPI/src/main/java/com/example/GitHubAPI/GitHubAPIController.java
+++ 2021_11/GitHubAPI/src/main/java/com/example/GitHubAPI/GitHubAPIController.java
@@ -1,8 +1,30 @@
 package com.example.GitHubAPI;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
 public class GitHubAPIController {
     
-    
+    @GetMapping("/allusers")
+    public GitHubAPI[] gitHubAPIInfo(RestTemplate restTemplate) {
+        String URL = "https://api.github.com/users";
+
+        GitHubAPI[] gitHubAPI = restTemplate.getForObject(URL, GitHubAPI[].class);
+
+        return gitHubAPI;
+    }
+
+    @GetMapping("/oneuser/{login}")
+    public GitHubAPI gitHubInfo(RestTemplate restTemplate, @PathVariable String login) {
+        String URL2 = "https://api.github.com/users/" + login;
+
+        GitHubAPI gitHubAPI2 = restTemplate.getForObject(URL2, GitHubAPI.class);
+
+        return gitHubAPI2;        
+    }
 
 
 }
--- 2021_11/GitHubAPI/src/main/java/com/example/GitHubAPI/GitHubApiApplication.java
+++ 2021_11/GitHubAPI/src/main/java/com/example/GitHubAPI/GitHubApiApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@RestController
 public class GitHubApiApplication {
 
 	public static void main(String[] args) {
