10f4a429659d960d0e8051c81c4fdcd8a86b6917
Simon Morris <simonjmorris213@gmail.com>
2021-12-05 00:35:14 -0500

    commit

---
+++ 2021_12/NewServerFromVideo/src/test/java/com/example/NewServerFromVideo/NewServerFromVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NewServerFromVideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewServerFromVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
