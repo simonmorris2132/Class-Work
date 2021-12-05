4bd432dc0837946370731fb5170112ed3001098a
Simon Morris <simonjmorris213@gmail.com>
2021-12-05 01:00:40 -0500

    commit

--- 2021_12/NewServerForTesting/src/main/java/com/example/NewServerForTesting/NewServerForTestingApplication.java
+++ 2021_12/NewServerForTesting/src/main/java/com/example/NewServerForTesting/NewServerForTestingApplication.java
@@ -1,13 +1,23 @@
 package com.example.NewServerForTesting;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NewServerForTestingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewServerForTestingApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public List<String> hello() {
+		return List.of("Hello", "World");
 	}
 
 }
