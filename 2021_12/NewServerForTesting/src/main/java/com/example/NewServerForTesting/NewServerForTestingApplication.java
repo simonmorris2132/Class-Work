3a127acd79a4cbc72080c9ce7327ba5563630a31
Simon Morris <simonjmorris213@gmail.com>
2021-12-05 01:17:26 -0500

    commit

--- 2021_12/NewServerForTesting/src/main/java/com/example/NewServerForTesting/NewServerForTestingApplication.java
+++ 2021_12/NewServerForTesting/src/main/java/com/example/NewServerForTesting/NewServerForTestingApplication.java
@@ -1,14 +1,7 @@
 package com.example.NewServerForTesting;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
-import com.example.NewServerForTesting.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class NewServerForTestingApplication {
--- 2021_12/NewServerForTesting/src/main/java/com/example/NewServerForTesting/Student/StudentController.java
+++ 2021_12/NewServerForTesting/src/main/java/com/example/NewServerForTesting/Student/StudentController.java
@@ -4,12 +4,16 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
-    @GetMapping("/")
-	public List<Student> rootRoute() {
+    
+    @GetMapping
+	public List<Student> getStudents() {
 		return List.of(new Student(1L, "Mariam", 30, LocalDate.of(2000, Month.FEBRUARY, 5), "mariam.jamal@gmail.com"));
 	}
+    
 }
