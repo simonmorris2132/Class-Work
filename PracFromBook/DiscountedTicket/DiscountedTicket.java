2242ac594be6877ebceceeaa41e1f0f1d182611f
Simon Morris <simonjmorris213@gmail.com>
2021-12-07 18:38:19 -0500

    commit

--- PracFromBook/DiscountedTicket/DiscountedTicket.java
+++ PracFromBook/DiscountedTicket/DiscountedTicket.java
@@ -10,8 +10,23 @@ public class DiscountedTicket {
        System.out.println("How old are you?");
        age = scanner.nextInt();
 
+       System.out.println("Have a Coupon? Y/N?");
+       reply = scanner.findWithinHorizon(".", 0).charAt(0);
+
        if (age >= 12 && age < 65) {
            price = 9.25;
+       } else if (age < 12 || age >= 65) {
+           price = 5.25;
        }
+
+       if (reply == 'Y' || reply == 'y') {
+           price -= 2.00;
+       } else if (reply != 'Y' && reply != 'y' && reply != 'n' && reply != 'N') {
+           System.out.println("huh?");
+       }
+
+       System.out.println("Please Pay $" + price + ".");
+       System.out.print("Enjoy the show!");
+       scanner.close();
    } 
 }
