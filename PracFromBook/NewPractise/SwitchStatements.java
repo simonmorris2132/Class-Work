46eb9ea73a127b27a1bcf161cd447bd6a04293f9
Simon Morris <simonjmorris213@gmail.com>
2021-12-09 18:55:24 -0500

    commit

--- PracFromBook/NewPractise/SwitchStatements.java
+++ PracFromBook/NewPractise/SwitchStatements.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class SwitchStatements {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int randomNumber = 0;
+
+        System.out.println("Type out your question: ");
+        scanner.nextLine();
+
+        switch (randomNumber) {
+            case 1:
+                System.out.println("What?");
+                break;
         
+            case 2:
+                System.out.println("No");
+                break;
+
+            default:
+                break;
+        }
     }
 }
