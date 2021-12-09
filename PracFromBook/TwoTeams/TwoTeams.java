cae42c329e05f368d137466a3586d88723bf0553
Simon Morris <simonjmorris213@gmail.com>
2021-12-09 00:58:09 -0500

    commit

--- PracFromBook/TwoTeams/TwoTeams.java
+++ PracFromBook/TwoTeams/TwoTeams.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 @SuppressWarnings("ALL")
 
 public class TwoTeams {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int hankee, socks;
+
+        System.out.print("Hankees and Socks scores? ");
+    }
 }
