b697c476d4287c99b5012a59c0471d75fb7ca06e
Simon Morris <simonjmorris213@gmail.com>
2021-12-09 20:58:33 -0500

    message

--- 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUserController.java
+++ 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUserController.java
@@ -4,8 +4,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("api/users")
 public class JPUserController {
     
 @GetMapping("/allusers")
@@ -24,6 +28,12 @@ public JPUser jpUserinfo2(RestTemplate restTemplate, @PathVariable short id) {
     JPUser jpUsers = restTemplate.getForObject(URL2, JPUser.class);
     
     return jpUsers;
+}
+
+@PostMapping("/create")//http://localhost:4000/api/users/create
+public JPUser createUser(RestTemplate restTemplate, @RequestBody JPUser userData) {
+    String URL = "https://jsonplaceholder.typicode.com/users";
+    
 }
 
 
