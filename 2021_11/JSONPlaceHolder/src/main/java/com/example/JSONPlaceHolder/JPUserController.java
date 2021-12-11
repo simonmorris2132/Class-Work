777f38461d34a926a80767c19bf44b784192e51b
Simon Morris <simonjmorris213@gmail.com>
2021-12-11 16:08:23 -0500

    commit

--- 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUserController.java
+++ 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUserController.java
@@ -2,12 +2,8 @@ package com.example.JSONPlaceHolder;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.URL;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.HttpHeaders;
-
 
 @RestController
 @RequestMapping("api/users")
