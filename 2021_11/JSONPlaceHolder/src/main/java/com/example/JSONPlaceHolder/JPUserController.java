c44a43dd698a84b28c0e9b37217f5e7571cba2ae
Simon Morris <simonjmorris213@gmail.com>
2021-12-11 00:32:20 -0500

    message

--- 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUserController.java
+++ 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUserController.java
@@ -2,11 +2,21 @@ package com.example.JSONPlaceHolder;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URL;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpHeaders;
+
 
 @RestController
 @RequestMapping("api/users")
@@ -33,7 +43,37 @@ public JPUser jpUserinfo2(RestTemplate restTemplate, @PathVariable short id) {
 @PostMapping("/create")//http://localhost:4000/api/users/create
 public JPUser createUser(RestTemplate restTemplate, @RequestBody JPUser userData) {
     String URL = "https://jsonplaceholder.typicode.com/users";
+
+    HttpEntity<JPUser> request = new HttpEntity(userData);
+
+    JPUser newUserData = restTemplate.postForObject(URL, request, JPUser.class);
+
+    return newUserData;
+}
+
+/* @PutMapping("/update/{id}")//http://localhost:4000/api/users/update
+public ResponseEntity<JPUser> updateUser(RestTemplate restTemplate, @RequestBody JPUser userData, @PathVariable short id) {
+    String URL = "https://jsonplaceholder.typicode.com/users/" + id;
     
+    HttpEntity<JPUser> request = new HttpEntity(userData);
+
+    ResponseEntity<JPUser> newUserData = restTemplate.put(URL, request);
+    //ResponseEntity<JPUser> newUserData = restTemplate.exchange(URL, HttpMethod.PUT, request, JPUser.class);
+
+    return newUserData;
+} */
+
+@DeleteMapping("/delete/{id}")
+public String deleteUser(RestTemplate restTemplate, @PathVariable short id) {
+    String URL = "https://jsonplaceholder.typicode.com/users/" + id;
+
+    HttpHeaders headers = new HttpHeaders();
+
+    HttpEntity request = new HttpEntity(headers);
+    
+
+    restTemplate.exchange(URL, HttpMethod.DELETE, request, JPUser.class);
+    return "deleted user with the id " + id;
 }
 
 
