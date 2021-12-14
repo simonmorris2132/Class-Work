d3933babb649f4eec92f1a0539f7eef5c6809036
Simon Morris <simonjmorris213@gmail.com>
2021-12-13 22:21:51 -0800

    commit

--- 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUser.java
+++ 2021_11/JSONPlaceHolder/src/main/java/com/example/JSONPlaceHolder/JPUser.java
@@ -5,6 +5,37 @@ public class JPUser {
     private String name;
     private String username;
     private String email;
+    private Address address;
+
+    public static class Address {
+        private String street;
+        private String suite;
+        private String city;
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getSuite() {
+            return suite;
+        }
+
+        public void setSuite(String suite) {
+            this.suite = suite;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+    }
 
     public short getId() {
         return this.id;
@@ -36,6 +67,14 @@ public class JPUser {
 
     public void setEmail(String email) {
         this.email = email;
-    } 
+    }
 
+    public Address getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }    
+    
 }
