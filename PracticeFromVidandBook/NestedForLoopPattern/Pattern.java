695238a0e99738f5e17c906acafd6b0124d073eb
Simon Morris <simonjmorris213@gmail.com>
2021-12-18 17:54:08 -0500

    commit

--- PracticeFromVidandBook/NestedForLoopPattern/Pattern.java
+++ PracticeFromVidandBook/NestedForLoopPattern/Pattern.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class Pattern {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("How many stars? :)");
+        Scanner scanner = new Scanner(System.in);
+        int numOfStars = scanner.nextInt();
+        
+        for (int i = 1; i <= numOfStars; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        for (int i = 4; i > 0; i--) {
+        for (int i = numOfStars; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
