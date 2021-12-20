4e8a4042b3cbdb4d65be96340a64f08632f13684
Simon Morris <simonjmorris213@gmail.com>
2021-12-19 21:30:48 -0500

    commit

--- PracticeFromVidandBook/ArrowKeyDetect/ArrowKeyDetect.java
+++ PracticeFromVidandBook/ArrowKeyDetect/ArrowKeyDetect.java
@@ -1,4 +1,4 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class ArrowKeyDetect {
 
@@ -8,6 +8,25 @@ public class ArrowKeyDetect {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.setFocusable(true);
+
+        JPanel panel = new JPanel();
+        JLabel up = new JLabel();
+        JLabel down = new JLabel();
+        JLabel left = new JLabel();
+        JLabel right = new JLabel();
+
+        panel.add(up);
+        panel.add(down);
+        panel.add(left);
+        panel.add(right);
+        frame.add(panel);
+
+        up.setText("Up: 0");
+        down.setText("Down: 0");
+        left.setText("Left: 0");
+        right.setText("Right: 0");
+
+
     }
 
     public static void main(String[] args) {
