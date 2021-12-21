5c4b516bffeadea909ae6a4977d88bbc39bfa2b8
Simon Morris <simonjmorris213@gmail.com>
2021-12-21 15:14:42 -0800

    commit

---
+++ 2021_12/testEmailBackend/src/main/java/com/example/testEmailBackend/TestEmailBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.testEmailBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEmailBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEmailBackendApplication.class, args);
+	}
+
+}
