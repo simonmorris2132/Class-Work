dc5f550d9cae795645c2e69d855af27b74278544
Simon Morris <simonjmorris213@gmail.com>
2021-12-29 23:02:00 -0800

    commit

--- PracticeFromVidandBook/PrimeNumbers/PrimeNumbers.java
+++ PracticeFromVidandBook/PrimeNumbers/PrimeNumbers.java
@@ -4,27 +4,29 @@ import java.util.ArrayList;
 
 public class PrimeNumbers {
     public static void main(String[] args) {
-        findPrimes(1, 100);
+        System.out.println(findPrimes(1, 100));
     }
 
-    public static ArrayList<Integer> findPrimes (int start, int end) {
+    public static ArrayList<Integer> findPrimes(int start, int end) {
         ArrayList<Integer> primes = new ArrayList<Integer>();
 
-        int n = 4;
-        boolean prime = true;
+        for (int n = start; n < end; n++) {
+            boolean prime = true;
 
-        int i = 2;
-        while (i <= n / 2) {
-            if (n % i == 0) {
-                prime = false;
-                break;
+            int i = 2;
+            while (i <= n / 2) {
+                if (n % i == 0) {
+                    prime = false;
+                    break;
+                }
+                i++;
             }
-            i++;
+            if(prime) {
+                primes.add(n);
+            }
         }
-
-        System.out.println(prime);
-
-        return null;
+ 
+        return primes;
     }
 
 }
