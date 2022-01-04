7a31da2e6c20fc71bf31f38ff8cf713fdffbbd44
Simon Morris <simonjmorris213@gmail.com>
2022-01-04 14:41:03 -0500

    Commit

--- 2022_01/projects/projectone/ProjectOne.java
+++ 2022_01/projects/projectone/ProjectOne.java
@@ -1,5 +1,10 @@
 public class ProjectOne {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(run("Simon", "Morris"));
     }
+
+    static String run(String first, String last) {
+        return first + " " + last;
+    }
+
 }
