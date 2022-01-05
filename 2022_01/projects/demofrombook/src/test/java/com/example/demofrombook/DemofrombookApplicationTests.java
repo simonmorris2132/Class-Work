97e28903191fe212de95b8e479cb3748e901b72d
Simon Morris <simonjmorris213@gmail.com>
2022-01-05 17:35:53 -0500

    commit

---
+++ 2022_01/projects/demofrombook/src/test/java/com/example/demofrombook/DemofrombookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demofrombook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemofrombookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
