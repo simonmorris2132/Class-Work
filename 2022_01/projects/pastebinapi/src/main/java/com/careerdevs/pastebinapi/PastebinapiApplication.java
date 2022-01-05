30a3fa5f4b16032f6ce0349468e9256288426378
Simon Morris <simonjmorris213@gmail.com>
2022-01-04 19:40:42 -0500

    commit

--- 2022_01/projects/pastebinapi/src/main/java/com/careerdevs/pastebinapi/PastebinapiApplication.java
+++ 2022_01/projects/pastebinapi/src/main/java/com/careerdevs/pastebinapi/PastebinapiApplication.java
@@ -2,6 +2,11 @@ package com.careerdevs.pastebinapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
