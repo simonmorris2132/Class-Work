400b42b877410d22f026e8c48a3498ff977a7ba9
Simon Morris <simonjmorris213@gmail.com>
2022-01-05 17:48:09 -0500

    commit

--- 2022_01/projects/demofrombook/src/main/java/com/example/demofrombook/controller/HelloWorld.java
+++ 2022_01/projects/demofrombook/src/main/java/com/example/demofrombook/controller/HelloWorld.java
@@ -1,8 +1,14 @@
 package com.example.demofrombook.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
     
+    @GetMapping("/sayhello")
+    public String sayHelloWorld() {
+        return "Hello World!";
+    }
+
 }
