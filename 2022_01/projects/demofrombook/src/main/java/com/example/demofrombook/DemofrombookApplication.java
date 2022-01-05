0fbde95cbe4fe46aea12ff6b953148cc9a3f9f5c
Simon Morris <simonjmorris213@gmail.com>
2022-01-05 17:22:17 -0500

    commit

---
+++ 2022_01/projects/demofrombook/src/main/java/com/example/demofrombook/DemofrombookApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofrombook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofrombookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemofrombookApplication.class, args);
+	}
+
+}
