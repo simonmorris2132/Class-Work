98b36f06bc733ff5e83ddb1385eec1ba24f8cebd
Simon Morris <simonjmorris213@gmail.com>
2022-01-04 20:02:31 -0500

    commit

--- 2022_01/projects/pastebinapi/src/main/java/com/careerdevs/pastebinapi/controllers/PastebinController.java
+++ 2022_01/projects/pastebinapi/src/main/java/com/careerdevs/pastebinapi/controllers/PastebinController.java
@@ -5,7 +5,6 @@ import com.careerdevs.pastebinapi.models.PastebinapiModel;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,13 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class PastebinController {
     
     @PostMapping("/create")
-    public ResponseEntity<PastebinapiModel> postMethodName(RestTemplate restTemplate, @RequestBody PastebinapiModel entity) {
+    public PastebinapiModel postMethodName(RestTemplate restTemplate, @RequestBody PastebinapiModel entity) {
         
         String url = "https://pastebin.com/api/api_post.php";
 
         HttpEntity<PastebinapiModel> request = new HttpEntity(entity);
 
-        ResponseEntity<PastebinapiModel> model = restTemplate.postForEntity(url, request, PastebinapiModel.class);
+        PastebinapiModel model = restTemplate.postForObject(url, request, PastebinapiModel.class);
 
         return model;
     }
