13c04f548d291dbfaea2f27c1bb7becde8ba8153
Simon Morris <simonjmorris213@gmail.com>
2022-01-06 21:15:32 -0500

    commit

---
+++ 2022_01/projects/webplaygroundfrombook/src/main/java/com/example/webplaygroundfrombook/WebplaygroundfrombookApplication.java
@@ -0,0 +1,13 @@
+package com.example.webplaygroundfrombook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebplaygroundfrombookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebplaygroundfrombookApplication.class, args);
+	}
+
+}
---
+++ 2022_01/projects/webplaygroundfrombook/src/main/java/com/example/webplaygroundfrombook/controllers/Controller.java
@@ -0,0 +1,6 @@
+package com.example.webplaygroundfrombook.controllers;
+
+
+public class Controller {
+    
+}
