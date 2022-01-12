2cf2f7b20854236f1db196834902a241906d57d7
Simon Morris <simonjmorris213@gmail.com>
2022-01-11 22:14:11 -0800

    commit

--- MIT_Lectures/lecturetwo/Pencil.java
+++ MIT_Lectures/lecturetwo/Pencil.java
@@ -1,12 +1,38 @@
 package lecturetwo;
 
 public class Pencil {
-    private String color = "red";
+    private String color;
     private int length;
     private float diameter;
-    private static long nextID = 0;
 
-    public void setColor(String newColor) {
-        color = newColor;
+    public Pencil(String color, int length, float diameter) {
+        this.color = color;
+        this.length = length;
+        this.diameter = diameter;
     }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getDiameter() {
+        return this.diameter;
+    }
+
+    public void setDiameter(float diameter) {
+        this.diameter = diameter;
+    }
+
 }
