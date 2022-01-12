4663fb81a0437e2a4108463c8a358b941fb44f7b
Simon Morris <simonjmorris213@gmail.com>
2022-01-11 22:18:10 -0800

    commit

--- MIT_Lectures/lecturetwo/Pencil.java
+++ MIT_Lectures/lecturetwo/Pencil.java
@@ -4,7 +4,7 @@ public class Pencil {
     private String color;
     private int length;
     private float diameter;
-    private static long nextID;
+    private static long nextID = 0;
 
     public Pencil(String color, int length, float diameter) {
         this.color = color;
