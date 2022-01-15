5631e2c8bb2f64c144f88024476c6fc6dabcd77a
Simon Morris <simonjmorris213@gmail.com>
2022-01-14 22:55:31 -0800

    Main

--- MIT_Lectures/lecturetwo/lecturetwo/Main.java
+++ MIT_Lectures/lecturetwo/lecturetwo/Main.java
@@ -1,6 +1,6 @@
 package lecturetwo;
 
-public class CreatePencil {
+public class Main {
     public static void main(String[] args) {
         Pencil p1 = new Pencil("red", 10, 13);
         System.out.println(p1);   
