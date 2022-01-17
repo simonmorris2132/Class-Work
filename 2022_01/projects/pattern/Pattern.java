6340b28bcc5fc6c71524bf475ad119dd788da855
Simon Morris <simonjmorris213@gmail.com>
2022-01-16 16:59:35 -0800

    commit

--- 2022_01/projects/pattern/Pattern.java
+++ 2022_01/projects/pattern/Pattern.java
@@ -1,7 +1,26 @@
 package projects.pattern;
 
+import java.util.*;
+
 public class Pattern {
     public static void main(String[] args) {
+        System.out.println("How many stars do you want to make a pattern out of?");
+        Scanner scanner = new Scanner(System.in);
+        int s = scanner.nextInt();
 
+        for (int i = 0; i <= s; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = s - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scanner.close();
     }
 }
