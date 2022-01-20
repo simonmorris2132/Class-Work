0eae4cc17a0f304c9a4a34e0b64b4645211f256b
Simon Morris <simonjmorris213@gmail.com>
2022-01-20 15:01:38 -0800

    commit

--- 2022_01/userinterface/UserInterface.java
+++ 2022_01/userinterface/UserInterface.java
@@ -1,7 +1,9 @@
 package userinterface;
 
+import java.util.*;
+
 public class UserInterface {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
     }
 }
