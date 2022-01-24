3483279811b228352bec0ab14511bb5426c91a62
Simon Morris <simonjmorris213@gmail.com>
2022-01-24 15:44:19 -0800

    commit

--- 2022_01/factory/CompanyMain.java
+++
@@ -1,5 +0,0 @@
-package factory;
-
-public class CompanyMain {
-    Company com1 = new Company("Bank of America", 1784, 364.60);
-}
---
+++ 2022_01/factory/factory/CompanyMain.java
@@ -0,0 +1,5 @@
+package factory;
+
+public class CompanyMain {
+    Company com1 = new Company("Bank of America", (short) 1784, 364.60);
+}
