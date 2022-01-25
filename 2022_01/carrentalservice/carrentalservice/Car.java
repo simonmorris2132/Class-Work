1af7feea43aa4e2c49fdffa3bface52486396427
Simon Morris <simonjmorris213@gmail.com>
2022-01-24 17:53:46 -0800

    commit

---
+++ 2022_01/carrentalservice/carrentalservice/Car.java
@@ -0,0 +1,42 @@
+package carrentalservice;
+
+public class Car {
+    private String make;
+    private String model;
+    private boolean isRented;
+
+    public Car(String make, String model, boolean isRented) {
+        this.make = make;
+        this.model = model;
+        this.isRented = isRented;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public boolean isIsRented() {
+        return this.isRented;
+    }
+
+    public boolean getIsRented() {
+        return this.isRented;
+    }
+
+    public void setIsRented(boolean isRented) {
+        this.isRented = isRented;
+    }
+
+}
--- 2022_01/factory/factory/SongMain.java
+++ 2022_01/factory/factory/SongMain.java
@@ -1,5 +1,5 @@
 package factory;
 
-public class Song {
+public class SongMain {
     
 }
