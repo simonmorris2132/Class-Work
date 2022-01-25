cb78a243a388a19eb39b1cadcf1586fbdda19f39
Simon Morris <simonjmorris213@gmail.com>
2022-01-24 18:23:49 -0800

    COMMIT

--- 2022_01/carrentalservice/carrentalservice/Car.java
+++ 2022_01/carrentalservice/carrentalservice/Car.java
@@ -3,12 +3,10 @@ package carrentalservice;
 public class Car {
     private String make;
     private String model;
-    private boolean isRented;
 
-    public Car(String make, String model, boolean isRented) {
+    public Car(String make, String model) {
         this.make = make;
         this.model = model;
-        this.isRented = isRented;
     }
 
     public String getMake() {
@@ -27,24 +25,11 @@ public class Car {
         this.model = model;
     }
 
-    public boolean isIsRented() {
-        return this.isRented;
-    }
-
-    public boolean getIsRented() {
-        return this.isRented;
-    }
-
-    public void setIsRented(boolean isRented) {
-        this.isRented = isRented;
-    }
-
     @Override
     public String toString() {
         return "{" +
             " make= '" + getMake() + "'" +
             ", model= '" + getModel() + "'" +
-            ", isRented= '" + isIsRented() + "'" +
             "}";
     }
 
--- 2022_01/carrentalservice/carrentalservice/RentalService.java
+++ 2022_01/carrentalservice/carrentalservice/RentalService.java
@@ -1,5 +1,7 @@
 package carrentalservice;
 
 public class RentalService {
-    
+    public static void main(String[] args) {
+        Car car1 = new Car("chevy", "camero");
+    }
 }
