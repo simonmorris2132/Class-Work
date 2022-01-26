50f49fb3910331fe6d62990413eb0a0eb88cc028
Simon Morris <simonjmorris213@gmail.com>
2022-01-26 13:52:13 -0800

    commit

--- 2022_01/factory/factory/SongMain.java
+++ 2022_01/factory/factory/SongMain.java
@@ -1,7 +1,12 @@
 package factory;
 
+import java.util.Scanner;
+
 public class SongMain {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is the Title of the song?");
+        String songTitle = scanner.nextLine();
     }
 }
