1202137f65b81ac3431a2e5272a0841cb4aa1d70
Simon Morris <simonjmorris213@gmail.com>
2022-01-27 14:57:11 -0800

    commit

--- 2022_01/factory/factory/SongMain.java
+++ 2022_01/factory/factory/SongMain.java
@@ -8,5 +8,20 @@ public class SongMain {
 
         System.out.println("What is the Title of the song?");
         String songTitle = scanner.nextLine();
+
+        System.out.println("Who is the Artist?");
+        String songArtist = scanner.nextLine();
+
+        System.out.println("What is the genre");
+        String songGenre = scanner.nextLine();
+
+        System.out.println("How long is the song in seconds?");
+        int songSec = scanner.nextInt();
+        scanner.nextLine();
+
+        scanner.close();
+
+        Song song = new Song(songTitle, songArtist, songGenre, songSec);
+        System.out.println(song);
     }
 }
