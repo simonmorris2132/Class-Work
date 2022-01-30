fe326618fcca2a9dc3d7238511e62fdac64151bc
Simon Morris <simonjmorris213@gmail.com>
2022-01-30 18:23:05 -0500

    commit

--- 2022_01/carrentalservicetwo/Main.java
+++ 2022_01/carrentalservicetwo/Main.java
@@ -2,7 +2,11 @@ package carrentalservicetwo;
 
 public class Main {
     public static void main(String[] args) {
-        CLI.readInt("What's your age?", 1, 99);
-        
+        System.out.println("Welcome to the Menu program!\n\n");
+
+        System.out.println("Select an option");
+        System.out.println("1) Menu 1");
+        System.out.println("2) Menu 2");
+        System.out.println("3) Exit");
     }
 }
