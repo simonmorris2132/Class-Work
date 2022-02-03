6524727ae78188f36c0d87b9e98fd9f4b02d5226
Simon Morris <simonjmorris213@gmail.com>
2022-02-02 23:08:53 -0800

    commit

--- 2022_01/com/careerdevs/algocli/UI.java
+++ 2022_01/com/careerdevs/algocli/UI.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class UI {
     private static Scanner scanner = new Scanner(System.in);
 
-
     public static String readString(String question) {
         System.out.print(question + "\nInput: ");
         return scanner.nextLine();
