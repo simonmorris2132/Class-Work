5796745d53c1de01d25f48b9b3d58c4fa49a5bb4
Simon Morris <simonjmorris213@gmail.com>
2022-02-03 13:35:43 -0800

    commit

--- 2022_01/com/careerdevs/algocli/UI.java
+++ 2022_01/com/careerdevs/algocli/UI.java
@@ -10,6 +10,30 @@ public class UI {
         return scanner.nextLine();
     }
 
+    public static int readInt(String question, int min, int max) {
+        while (true) {
+            try {
+                System.out.println(question);
+                System.out.print("Number (" + min + " - " + max + "): ");
 
+                int userInt = scanner.nextInt();
+                scanner.nextLine();
+
+                if (userInt >= min && userInt <= max) {
+                    return userInt;
+                }
+                
+            } catch (InputMismatchException e) {
+                System.out.println("You must enter a whole number, try again");
+                scanner.nextLine();
+            
+            } catch (Exception e) {
+                System.out.println("Error occured");
+                System.out.println(e + "\n" + e.getMessage());
+                return 0;
+            }
+
+        }
+    }
     
 }
