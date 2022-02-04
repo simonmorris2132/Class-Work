6279599b551fb87b03c2951836d29cf67a6a03c9
Simon Morris <simonjmorris213@gmail.com>
2022-02-03 19:08:18 -0800

    commit

--- 2022_01/com/careerdevs/algocli/Main.java
+++ 2022_01/com/careerdevs/algocli/Main.java
@@ -2,6 +2,8 @@ package com.careerdevs.algocli;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to the Algorithm CLI");
+
         
     }
 }
