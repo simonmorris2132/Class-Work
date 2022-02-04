0a75433cb6b6b901891758bcaa49254f1edb822b
Simon Morris <simonjmorris213@gmail.com>
2022-02-03 19:12:55 -0800

    commit

--- 2022_01/com/careerdevs/algocli/Main.java
+++ 2022_01/com/careerdevs/algocli/Main.java
@@ -1,9 +1,21 @@
 package com.careerdevs.algocli;
 
 public class Main {
+
+    /* Reverse a string
+       How many vowels
+       two makes ten
+       capture the rook
+       get word count    
+    */
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Algorithm CLI");
-
-        
+        mainMenu();
     }
+
+    private static void mainMenu() {
+        System.out.println(UI.lineSpacer() + "\nMAIN MENU\n" + UI.lineSpacer());
+    }
+
 }
