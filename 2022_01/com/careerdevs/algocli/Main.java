c9274178f2803006d730037e856a36ea96528863
Simon Morris <simonjmorris213@gmail.com>
2022-02-03 19:06:42 -0800

    commit

--- 2022_01/com/careerdevs/algocli/Main.java
+++ 2022_01/com/careerdevs/algocli/Main.java
@@ -2,7 +2,6 @@ package com.careerdevs.algocli;
 
 public class Main {
     public static void main(String[] args) {
-        int userSelect = UI.readInt("Whats your age", 1, 99);
-        System.out.println(userSelect);
+        
     }
 }
