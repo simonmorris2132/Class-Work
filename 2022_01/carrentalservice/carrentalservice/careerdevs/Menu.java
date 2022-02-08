6a8a610be0e1e91a17b279f650e45a491332c3fc
Simon Morris <simonjmorris213@gmail.com>
2022-02-08 18:31:48 -0500

    commit

--- 2022_01/carrentalservice/carrentalservice/careerdevs/Menu.java
+++ 2022_01/carrentalservice/carrentalservice/careerdevs/Menu.java
@@ -42,11 +42,76 @@ public class Menu {
         }
     }
 
+    public void returnCar() {
+        if (rentList.size() == 0) {
+            System.out.println("\nWe have no cars out for rent! Returning to main menu...\n");
+            run();
+        }
+
+        while (rentList.size() != 0) {
+            inv.showRentedMenu();
+            
+            System.out.print("\n");
+            int rentSelection = input.getInt(1, rentList.size());
+
+            for (int i = 0; i < rentList.size(); i++) {
+                if (rentSelection - 1 == i) {
+                    System.out.println("\nThank you for returning the " + rentList.get(i) + "! We hope to see you again!");
+                    avaList.add(rentList.get(i));
+                    rentList.remove(i);
+                    subMenu();
+                }
+            }
+        }
+    }
+
     public void rentedTotal(int i) {
         System.out.println(i);
         System.out.println("\nThank you for choosing the " + avaList.get(i).getMake() + avaList.get(i).getModel() + "! How many days do you wish to rent it?\n");
         int select = input.getInt();
         System.out.println("For " + select + " days.");
+    }
+
+    public void subMenu() {
+        subMenuDialogue();
+        int subSelection = input.getInt(1, 5);
+        userSelection(subSelection);
+    }
+
+    private void subMenuDialogue() {
+        System.out.println("\nThank you for choosing the Car Rental Service! What would you like to do from this point?");
+        System.out.println("1) Restart this program and refresh the lot");
+        System.out.println("2) Return to the main menu.");
+        System.out.println("3) Go to the rental menu to rent a car.");
+        System.out.println("4) Go to the return menu to return a car.");
+        System.out.println("5) Exit the program");
+    }
+
+    public void userSelection(int selectionParameter) {
+        if (selectionParameter == 1) {
+            System.out.println("\nRestarting the program!\n");
+            avaList.clear();
+            rentList.clear();
+            start();
+        } else if (selectionParameter == 2) {
+            System.out.println("\nReturning to the main menu.\n");
+            run();
+        } else if (selectionParameter == 3) {
+            System.out.println("\nSending you to the rental menu\n");
+            renting();
+        } else if (selectionParameter == 4) {
+            System.out.println("\nSending you to the return menu\n");
+            returnCar();
+        } else {
+            new UserInput().exit();
+        }
+    }
+
+    public void returnTotal(int i) {
+        System.out.println(i);
+        System.out.println("\nThank you for choosing the " + avaList.get(i).getMake() + " " + avaList.get(i).getModel() + ". How many days would you like to rent it for?");
+        int days = input.getInt();
+        System.out.println("For a total of " + days);
     }
 
 
