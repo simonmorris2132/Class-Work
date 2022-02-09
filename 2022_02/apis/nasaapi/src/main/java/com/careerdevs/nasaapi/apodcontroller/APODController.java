1474af1fd547e82863f0f6130fea01495b182d84
Simon Morris <simonjmorris213@gmail.com>
2022-02-09 00:45:02 -0500

    commit

--- 2022_02/apis/nasaapi/src/main/java/com/careerdevs/nasaapi/apodcontroller/APODController.java
+++ 2022_02/apis/nasaapi/src/main/java/com/careerdevs/nasaapi/apodcontroller/APODController.java
@@ -1,8 +1,6 @@
 package com.careerdevs.nasaapi.apodcontroller;
 
 import com.careerdevs.nasaapi.apodmodel.APODModel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
