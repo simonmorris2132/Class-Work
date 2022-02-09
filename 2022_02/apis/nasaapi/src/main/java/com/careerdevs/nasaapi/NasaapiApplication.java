955730e655f5c2254225c6ba4d9199a022e80d15
Simon Morris <simonjmorris213@gmail.com>
2022-02-09 00:31:32 -0500

    commit

---
+++ 2022_02/apis/nasaapi/src/main/java/com/careerdevs/nasaapi/NasaapiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.nasaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaapiApplication.class, args);
+	}
+
+}
