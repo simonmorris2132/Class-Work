073ca4dbec5880d7d9d160398c7a32bbe6c715dd
Simon Morris <simonjmorris213@gmail.com>
2022-02-09 00:42:11 -0500

    commit

--- 2022_02/apis/nasaapi/src/main/java/com/careerdevs/nasaapi/apodcontroller/APODController.java
+++ 2022_02/apis/nasaapi/src/main/java/com/careerdevs/nasaapi/apodcontroller/APODController.java
@@ -1,5 +1,26 @@
 package com.careerdevs.nasaapi.apodcontroller;
 
+import com.careerdevs.nasaapi.apodmodel.APODModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
 public class APODController {
     
+    @Autowired
+    private Environment environment;
+    
+    @GetMapping("/apodinfo")
+    public APODModel apodInfo(RestTemplate restTemplate) {
+        String url = "https://api.nasa.gov/planetary/apod?" + environment.getProperty("MYAPIKEY");
+
+        APODModel apod = restTemplate.getForObject(url, APODModel.class);
+
+        return apod;
+    }
+
+
 }
