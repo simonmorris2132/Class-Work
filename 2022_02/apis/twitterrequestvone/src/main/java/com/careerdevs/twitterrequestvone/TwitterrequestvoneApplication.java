8c5bad98fdadc2287d57829196efb1b13d70425b
Simon Morris <simonjmorris213@gmail.com>
2022-02-11 00:24:51 -0500

    commit

---
+++ 2022_02/apis/twitterrequestvone/src/main/java/com/careerdevs/twitterrequestvone/TwitterrequestvoneApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.twitterrequestvone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterrequestvoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterrequestvoneApplication.class, args);
+	}
+
+}
