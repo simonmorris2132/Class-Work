1376157d3b309f2855b73ea891e3ce4fb5db9391
Simon Morris <simonjmorris213@gmail.com>
2022-02-17 16:04:02 -0500

    commit

---
+++ 2022_02/apis/twitterapivone/src/main/java/com/careerdevs/twitterapivone/TwitterapivoneApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.twitterapivone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterapivoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterapivoneApplication.class, args);
+	}
+
+}
