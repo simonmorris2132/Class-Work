63a654165e2f62daddcc2555a6e416299aaad942
Simon Morris <simonjmorris213@gmail.com>
2022-02-19 14:11:28 -0800

    twitter

--- 2022_02/apis/twitterapi/src/main/java/com/careerdevs/twitterapi/controllers/TwitterapiController.java
+++ 2022_02/apis/twitterapi/src/main/java/com/careerdevs/twitterapi/controllers/TwitterapiController.java
@@ -9,5 +9,7 @@ public class TwitterapiController {
     @GetMapping("/yeattwitter")
     public TwitterapiModel twitterInfo(RestTemplate restTemplate) {
         String url = "https://api.twitter.com/2/users/by/username/yeat1_ -H Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAA5wZAEAAAAA7MhNVTOspQzQcFJzlHm97PeZWvY%3DRNQ3uIkkW2n3Rwf464oJFoMVbv3aCCeqKG";
+
+        return null;
     }
 }
