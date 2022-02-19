ebbb714121417f09324fc69c8457ecf6aae4eac6
Simon Morris <simonjmorris213@gmail.com>
2022-02-19 14:11:06 -0800

    commit

--- 2022_02/apis/twitterapi/src/main/java/com/careerdevs/twitterapi/controllers/TwitterapiController.java
+++ 2022_02/apis/twitterapi/src/main/java/com/careerdevs/twitterapi/controllers/TwitterapiController.java
@@ -1,5 +1,13 @@
 package com.careerdevs.twitterapi.controllers;
 
+import com.careerdevs.twitterapi.models.TwitterapiModel;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
 public class TwitterapiController {
-    
+    @GetMapping("/yeattwitter")
+    public TwitterapiModel twitterInfo(RestTemplate restTemplate) {
+        String url = "https://api.twitter.com/2/users/by/username/yeat1_ -H Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAA5wZAEAAAAA7MhNVTOspQzQcFJzlHm97PeZWvY%3DRNQ3uIkkW2n3Rwf464oJFoMVbv3aCCeqKG";
+    }
 }
--- 2022_02/apis/twitterapi/src/main/java/com/careerdevs/twitterapi/models/TwitterapiModel.java
+++ 2022_02/apis/twitterapi/src/main/java/com/careerdevs/twitterapi/models/TwitterapiModel.java
@@ -1,5 +1,8 @@
 package com.careerdevs.twitterapi.models;
 
 public class TwitterapiModel {
-    
+    private String data;
+
+
+
 }
