ba077b93eb93617b89a9aaeec9ed47bbe7d2c1fc
Simon Morris <simonjmorris213@gmail.com>
2022-02-20 17:07:07 -0800

    commit

--- 2022_02/apis/twitterapi/src/main/java/com/careerdevs/twitterapi/models/TwitterapiModel.java
+++ 2022_02/apis/twitterapi/src/main/java/com/careerdevs/twitterapi/models/TwitterapiModel.java
@@ -3,6 +3,12 @@ package com.careerdevs.twitterapi.models;
 public class TwitterapiModel {
     private String data;
 
+    public String getData() {
+        return this.data;
+    }
 
+    public void setData(String data) {
+        this.data = data;
+    }
 
 }
