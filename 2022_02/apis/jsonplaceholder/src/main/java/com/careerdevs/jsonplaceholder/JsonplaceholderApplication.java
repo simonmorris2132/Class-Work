638dd222694224d3ebf6e3e7039acc1da93c1ba6
Simon Morris <simonjmorris213@gmail.com>
2022-03-03 14:58:24 -0800

    commit

---
+++ 2022_02/apis/jsonplaceholder/src/main/java/com/careerdevs/jsonplaceholder/JsonplaceholderApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.jsonplaceholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonplaceholderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonplaceholderApplication.class, args);
+	}
+
+}
