9429086190e0845c7c389509369e76ba3273e0e0
Simon Morris <simonjmorris213@gmail.com>
2022-03-11 02:32:51 -0600

    commit

---
+++ 2022_02/apis/stockapi/src/test/java/com/careerdevs/stockapi/StockapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.stockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
