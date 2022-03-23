43feb2fe12a7b43539eb11ae5bb1caa41036d9e5
Simon Morris <simonjmorris213@gmail.com>
2022-03-22 22:34:58 -0400

    commit

--- 2022_03/basicstuff/carrentalcli/src/Car.java
+++ 2022_03/basicstuff/carrentalcli/src/Car.java
@@ -1,3 +1,38 @@
 public class Car {
+    private String make;
+    private String model;
+    private String isRented;
     
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+        this.isRented = false;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+
+    public boolean isIsRented() {
+        return this.isRented;
+    }
+
+    public void setIsRented(boolean isRented) {
+        this.isRented = isRented;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " make = '" + getMake() + "'" +
+            ", model = '" + getModel() + "'" +
+            ", isRented = '" + isIsRented() + "'" +
+            "}";
+    }
+
 }
