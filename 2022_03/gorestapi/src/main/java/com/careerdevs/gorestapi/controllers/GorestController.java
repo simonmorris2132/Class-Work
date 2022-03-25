bfdb9af1bdff28ff65fa437a4f760d68a942bde7
Simon Morris <simonjmorris213@gmail.com>
2022-03-25 00:53:31 -0500

    commit

--- 2022_03/gorestapi/src/main/java/com/careerdevs/gorestapi/controllers/GorestController.java
+++ 2022_03/gorestapi/src/main/java/com/careerdevs/gorestapi/controllers/GorestController.java
@@ -1,14 +1,11 @@
 package com.careerdevs.gorestapi.controllers;
 
 import com.careerdevs.gorestapi.models.GorestModel;
-import com.careerdevs.gorestapi.models.GorestModelArray;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
