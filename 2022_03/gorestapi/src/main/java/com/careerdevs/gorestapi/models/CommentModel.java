6c156addf703ee7122ea3de7123d96a3c0e0ed5b
Simon Morris <simonjmorris213@gmail.com>
2022-03-29 07:02:08 -0500

    commit

---
+++ 2022_03/gorestapi/src/main/java/com/careerdevs/gorestapi/models/CommentModel.java
@@ -0,0 +1,54 @@
+package com.careerdevs.gorestapi.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CommentModel {
+    private int id;
+    @JsonProperty("post_id")
+    private int postId;    
+    private String name;
+    private String email;
+    private String body;
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostId() {
+        return this.postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
