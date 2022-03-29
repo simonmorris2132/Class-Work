c897ea036ffc50a3f2037431b9fba0362b7acc8c
Simon Morris <simonjmorris213@gmail.com>
2022-03-29 09:42:57 -0500

    commit

--- 2022_03/gorestapi/src/main/java/com/careerdevs/gorestapi/controllers/CommentController.java
+++ 2022_03/gorestapi/src/main/java/com/careerdevs/gorestapi/controllers/CommentController.java
@@ -3,21 +3,25 @@ package com.careerdevs.gorestapi.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import com.careerdevs.gorestapi.models.CommentModel;
-
-import org.apache.logging.log4j.spi.ObjectThreadContextMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/comment")
 public class CommentController {
     
+    @Autowired
+    Environment env;
+
     @GetMapping("/firstpage")
     public CommentModel[] getFirstPage(RestTemplate restTemplate) {
         String url = "https://gorest.co.in/public/v2/comments";
@@ -37,5 +41,47 @@ public class CommentController {
         }
 
     }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteOneComment(RestTemplate restTemplate, @PathVariable("id") int commentId) {
+        
+        try {
+            String url = "https://gorest.co.in/public/v2/comments/" + commentId;
+            String token = env.getProperty("GO_REST_TOKEN");
+            url += "?access-token=" + token;
+            CommentModel deletedComment = restTemplate.getForObject(url, CommentModel.class);
+
+            restTemplate.delete(url);
+
+            return new ResponseEntity<>(deletedComment, HttpStatus.OK);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Object> postComment(RestTemplate restTemplate, @RequestBody CommentModel newComment) {
+        try {
+            String url = "https://gorest.co.in/public/v2/comments/";
+            String token = env.getProperty("GO_REST_TOKEN");
+            url += "?access-token=" + token;
+
+            HttpEntity<CommentModel> request = new HttpEntity<CommentModel>(newComment);
+
+            CommentModel createdComment = restTemplate.postForObject(url, request, CommentModel.class);
+
+            return new ResponseEntity<>(createdComment, HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
