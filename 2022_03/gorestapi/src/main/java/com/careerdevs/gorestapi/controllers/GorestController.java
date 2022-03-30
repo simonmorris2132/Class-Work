7d2bd9169a477e2d6e13dd94092ab58eed95d6fe
Simon Morris <simonjmorris213@gmail.com>
2022-03-29 20:02:23 -0500

    commit

--- 2022_03/gorestapi/src/main/java/com/careerdevs/gorestapi/controllers/GorestController.java
+++ 2022_03/gorestapi/src/main/java/com/careerdevs/gorestapi/controllers/GorestController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,6 +57,28 @@ public class GorestController {
         try {
             String url = "https://gorest.co.in/public/v2/comments/" + userId;
             return new ResponseEntity<>(restTemplate.getForObject(url, GorestModel.class), HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteOneUser(RestTemplate restTemplate, @PathVariable("id") int userId) {
+        
+        try {
+            String url = "https://gorest.co.in/public/v2/comments/" + userId;
+            String token = env.getProperty("GO_REST_TOKEN");
+            url += "?access-token=" + token;
+            GorestModel deletedUser = restTemplate.getForObject(url, GorestModel.class);
+
+            restTemplate.delete(url);
+
+            return new ResponseEntity<>(deletedUser, HttpStatus.OK);
+
         } catch (Exception e) {
             System.out.println(e.getClass());
             System.out.println(e.getMessage());
