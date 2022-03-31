f2f6f3b9ec8a4c5f93a8bd6c7b88f4ec05e25345
Simon Morris <simonjmorris213@gmail.com>
2022-03-31 17:38:31 -0500

    copmmit

--- 2022_03/sqldata/sqldata/src/main/java/com/careerdevs/sqldata/User.java
+++ 2022_03/sqldata/sqldata/src/main/java/com/careerdevs/sqldata/User.java
@@ -10,4 +10,34 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    private String name;
+
+    private String email;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 }
