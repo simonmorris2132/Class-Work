ed7ffe073a71b5012819b783102823ca18875eb1
Simon Morris <simonjmorris213@gmail.com>
2022-03-31 17:22:58 -0500

    commit

---
+++ 2022_03/sqldata/sqldata/src/main/java/com/careerdevs/sqldata/SqldataApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.sqldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqldataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqldataApplication.class, args);
+	}
+
+}
---
+++ 2022_03/sqldata/sqldata/src/test/java/com/careerdevs/sqldata/SqldataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.sqldata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqldataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
