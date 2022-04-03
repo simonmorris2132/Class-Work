fec9885f1a0a559d1491f4fc59bfe6e8feaaa7ec
Simon Morris <simonjmorris213@gmail.com>
2022-04-03 01:58:24 -0500

    commit

--- 2022_03/registration/src/main/java/com/careerdevs/registration/appuser/AppUser.java
+++ 2022_03/registration/src/main/java/com/careerdevs/registration/appuser/AppUser.java
@@ -1,52 +1,64 @@
 package com.careerdevs.registration.appuser;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class AppUser implements UserDetails {
 
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private AppUserRole appUserRole;
+    private boolean locked;
+    private boolean enabled;
+
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        return null;
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singletonList(authority);
     }
 
     @Override
     public String getPassword() {
         // TODO Auto-generated method stub
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
         // TODO Auto-generated method stub
-        return null;
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
         // TODO Auto-generated method stub
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
         // TODO Auto-generated method stub
-        return false;
+        return !locked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
         // TODO Auto-generated method stub
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
         // TODO Auto-generated method stub
-        return false;
+        return enabled;
     }
     
 }
---
+++ 2022_03/registration/src/main/java/com/careerdevs/registration/appuser/AppUserRole.java
@@ -0,0 +1,5 @@
+package com.careerdevs.registration.appuser;
+
+public enum AppUserRole {
+    USER, ADMIN
+}
