e81b34ecd629c26d157c8689cf50390003e2c95e
Simon Morris <simonjmorris213@gmail.com>
2022-04-03 06:55:42 -0500

    commit

--- 2022_03/registration/src/main/java/com/careerdevs/registration/security/config/WebSecurityConfig.java
+++ 2022_03/registration/src/main/java/com/careerdevs/registration/security/config/WebSecurityConfig.java
@@ -1,5 +1,29 @@
 package com.careerdevs.registration.security.config;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/api/v*/registration/**").permitAll().anyRequest().authenticated().and().formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider()
+    }
+
+    @Bean
+
 }
