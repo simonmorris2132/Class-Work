6349c348afc1213a73be715c28a654aebaccc47a
Simon Morris <simonjmorris213@gmail.com>
2022-04-12 18:04:07 -0500

    commit

---
+++ 2022_04/gorestsql/src/main/java/com/careerdevs/gorestsql/GorestsqlApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.gorestsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorestsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorestsqlApplication.class, args);
+	}
+
+}
---
+++ 2022_04/gorestsql/src/test/java/com/careerdevs/gorestsql/GorestsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.gorestsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorestsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
