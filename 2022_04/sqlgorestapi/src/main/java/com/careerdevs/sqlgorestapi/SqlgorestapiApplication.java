7576bd724329544f5ef8ba03406038decbbbcc8f
Simon Morris <simonjmorris213@gmail.com>
2022-04-12 17:18:26 -0500

    commit

---
+++ 2022_04/sqlgorestapi/src/main/java/com/careerdevs/sqlgorestapi/SqlgorestapiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.sqlgorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlgorestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlgorestapiApplication.class, args);
+	}
+
+}
---
+++ 2022_04/sqlgorestapi/src/test/java/com/careerdevs/sqlgorestapi/SqlgorestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.sqlgorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlgorestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
