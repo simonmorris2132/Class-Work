15d4437c1ef145a4402154b09a83ba4f63d392e2
Simon Morris <simonjmorris213@gmail.com>
2022-04-24 00:03:28 -0500

    commit

--- 2022_04/pystuff/game.py
+++ 2022_04/pystuff/game.py
@@ -1,8 +1,27 @@
 from tkinter import *
 
 root = Tk()
-root.geometry('800x600')
+root.geometry('1280x600')
 root.title("Minesweeper")
-root.resizable()
-root.mainloop()
+root.resizable(False, False)
 
+
+top_frame = Frame(
+    root,
+    background='red',
+    width=1280,
+    height=180
+)
+
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    background='blue',
+    width=360,
+    height=540
+)
+
+left_frame.place(x=0, y=180)
+
+root.mainloop()
