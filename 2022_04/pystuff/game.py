64654bacf738530f6a76fca7e2c8fb20ebcd5849
Simon Morris <simonjmorris213@gmail.com>
2022-04-24 21:45:43 -0500

    commit

--- 2022_04/pystuff/game.py
+++ 2022_04/pystuff/game.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import settings
+import utils
 
 root = Tk()
 root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
@@ -10,8 +11,8 @@ root.resizable(False, False)
 top_frame = Frame(
     root,
     background='red',
-    width=1280,
-    height=180
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
 )
 
 top_frame.place(x=0, y=0)
@@ -19,8 +20,8 @@ top_frame.place(x=0, y=0)
 left_frame = Frame(
     root,
     background='blue',
-    width=360,
-    height=540
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
 )
 
 left_frame.place(x=0, y=180)
