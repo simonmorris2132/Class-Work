5930ed3796a65a0b7fef22021033f23b39ba298e
Simon Morris <simonjmorris213@gmail.com>
2022-04-25 00:23:01 -0500

    commit

--- 2022_04/pystuff/game.py
+++ 2022_04/pystuff/game.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.tix import COLUMN
 import settings
 import utils
 from cell import Cell
@@ -29,23 +30,19 @@ left_frame.place(x=0, y=utils.height_prct(25))
 
 center_frame = Frame(
     root,
-    bg='black',
+    background='black',
     width=utils.width_prct(75),
     height=utils.height_prct(75)
 )
 
 center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
 
-c1 = Cell()
-c1.create_btn_object(center_frame)
-c1.cell_btn_object.grid(
-    column=0, row=0
-)
-
-c2 = Cell()
-c2.create_btn_object(center_frame)
-c2.cell_btn_object.grid(
-    column=0, row=1
-)
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x, row=y
+        )
 
 root.mainloop()
