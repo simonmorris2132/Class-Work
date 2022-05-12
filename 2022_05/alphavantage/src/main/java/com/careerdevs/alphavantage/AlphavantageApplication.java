3d48699670f2d968fb02b841ec40596be4171a84
Simon Morris <simonjmorris213@gmail.com>
2022-05-12 18:18:22 -0500

    commit

---
+++ 2022_05/alphavantage/src/main/java/com/careerdevs/alphavantage/AlphavantageApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.alphavantage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphavantageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphavantageApplication.class, args);
+	}
+
+}
---
+++ 2022_05/alphavantage/src/test/java/com/careerdevs/alphavantage/AlphavantageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.alphavantage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphavantageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
