4cfa5db201478e6485a65fb0d83b9154f6e47ea0
Simon Morris <simonjmorris213@gmail.com>
2022-05-18 01:42:53 -0500

    commit

---
+++ 2022_05/ghibliapi/src/main/java/com/careerdevs/ghibliapi/GhibliapiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.ghibliapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhibliapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhibliapiApplication.class, args);
+	}
+
+}
---
+++ 2022_05/ghibliapi/src/test/java/com/careerdevs/ghibliapi/GhibliapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.ghibliapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhibliapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
