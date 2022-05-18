7bb3be4d6e5aef83e2af5de3d797bf82e6ce2985
Simon Morris <simonjmorris213@gmail.com>
2022-05-18 01:49:15 -0500

    commit

---
+++ 2022_05/ghibliapi/src/main/java/com/careerdevs/ghibliapi/controllers/RootController.java
@@ -0,0 +1,16 @@
+package com.careerdevs.ghibliapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping("/")
+public class RootController {
+    
+    @GetMapping("/")
+    public ResponseEntity<String> rootRoute() {
+        return ResponseEntity.ok("Hello Ghibli Fans!");
+    }
+
+}
