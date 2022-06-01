d935c3b823051c9b8311023b5646262a04531983
Simon Morris <simonjmorris213@gmail.com>
2022-05-31 23:11:40 -0500

    commit

---
+++ 2022_05/weatherapiv1/src/main/java/com/careerdevs/weatherapiv1/Weatherapiv1Application.java
@@ -0,0 +1,13 @@
+package com.careerdevs.weatherapiv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Weatherapiv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Weatherapiv1Application.class, args);
+	}
+
+}
---
+++ 2022_05/weatherapiv1/src/test/java/com/careerdevs/weatherapiv1/Weatherapiv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.weatherapiv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Weatherapiv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
