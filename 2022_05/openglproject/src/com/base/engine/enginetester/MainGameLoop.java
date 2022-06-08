620144251b4cd4f159ab623f6a1e90996f35adca
Simon Morris <simonjmorris213@gmail.com>
2022-06-07 21:44:14 -0500

    commit

--- 2022_05/openglproject/src/com/base/engine/enginetester/MainGameLoop.java
+++ 2022_05/openglproject/src/com/base/engine/enginetester/MainGameLoop.java
@@ -2,6 +2,9 @@ package com.base.engine.enginetester;
 
 import org.lwjgl.opengl.Display;
 import com.base.engine.renderengine.DisplayManager;
+import com.base.engine.renderengine.Loader;
+import com.base.engine.renderengine.RawModel;
+import com.base.engine.renderengine.Renderer;
 
 public class MainGameLoop {
     
@@ -9,10 +12,28 @@ public class MainGameLoop {
         
         DisplayManager.createDisplay();
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        //OpenGL expects vertices to be defined counter clockwise by default
+        float[] vertices = {
+            -0.5f, 0.5f, 0f,
+            -0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f,
+            0.5f, 0.5f, 0f,
+            -0.5f, 0.5f, 0f
+          };
+
+          RawModel model = loader.loadToVAO(vertices);
+
         while (!Display.isCloseRequested()) {
+            renderer.prepare();
+            renderer.render(model);
             DisplayManager.updateDisplay();
         }
 
+        loader.cleanUp();
         DisplayManager.closeDisplay();
 
     }
